089d4c4dc3fcedba80c6acf2bd124489b93e6f89
Matt McAchran <matt.mcachran@webdevstudios.com>
2017-11-29 08:50:34 -0500

    Adds cache class.

---
+++ lib/class-cache.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * Handles caching.
+ *
+ * @package Basecamp RSS Feed Parser
+ */
+
+/**
+ * Cache.
+ */
+class Cache {
+	/**
+	 * Fetch tokens from cache
+	 *
+	 * @return object|bool Token object if true, false on failure.
+	 */
+	public static function fetch_tokens() {
+		$cache_file = BASE_DIR . '/cache/tokens.txt';
+
+		// Bail early if no cache.
+		if ( ! file_exists( $cache_file ) ) {
+			return false;
+		}
+
+		$cache = file_get_contents( $cache_file ); // @codingStandardsIgnoreLine
+
+		return json_decode( $cache );
+	}
+
+	/**
+	 * Cache tokens after successful request
+	 *
+	 * @param  object $tokens tokens object from API.
+	 */
+	public static function save_tokens( $tokens ) {
+		$cache_file = BASE_DIR . '/cache/tokens.txt';
+
+		// Bail early if no cache.
+		if ( ! file_exists( $cache_file ) ) {
+			return false;
+		}
+
+		file_put_contents( $cache_file, json_encode( $tokens ) ); // @codingStandardsIgnoreLine
+	}
+
+	/**
+	 * Get the last run timestamp.
+	 *
+	 * @return string Last run timestamp.
+	 */
+	public static function get_last_run_timestamp() {
+		$cache_file = BASE_DIR . '/cache/last_run.txt';
+
+		// Bail early if no cache.
+		if ( ! file_exists( $cache_file ) ) {
+			return false;
+		}
+
+		$cache = file_get_contents( $cache_file ); // @codingStandardsIgnoreLine
+
+		return $cache;
+	}
+
+	/**
+	 * Cache timestamp after successful request
+	 */
+	public static function save_last_run() {
+		$cache_file = BASE_DIR . '/cache/last_run.txt';
+
+		// Bail early if no cache.
+		if ( ! file_exists( $cache_file ) ) {
+			return false;
+		}
+
+		file_put_contents( $cache_file, strtotime( 'now' ) ); // @codingStandardsIgnoreLine
+	}
+}
