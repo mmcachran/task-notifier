c582dd7b2b3ea8f952d6a81055fea476b59b6362
mmcachran <mattmcachran@gmail.com>
2018-01-19 14:06:20 -0500

    Updates formatting.

--- ancient.php
+++ ancient.php
@@ -21,6 +21,13 @@ if ( file_exists( BASE_DIR . '/lib/class-autoloader.php' ) ) {
 	require_once BASE_DIR . '/lib/class-autoloader.php';
 }
 
+// Check for BC credentials to be defined.
+if ( ! defined( 'BC_CLIENT_ID' ) || ! defined( 'BC_CLIENT_SECRET' ) ) {
+	echo 'BC_CLIENT_ID or BC_CLIENT_SECRET not defined. Define these in a config file in the document root.';
+	exit( 0 );
+}
+
+
 // Initialize the BC old tasks class.
 $basecamp = \Basecamp\Basecamp_Old_Tasks::get_instance();
 
@@ -36,14 +43,53 @@ $project_tasks = $basecamp->get_old_tasks();
 
 <!DOCTYPE html>
 <html>
-	<head></head>
+	<head>
+		<link rel="stylesheet" href="assets/css/styles.css" /> <?php // @codingStandardsIgnoreLine ?>
+		<script src="assets/js/vendor/jquery.min.js" type="text/javascript"> <?php // @codingStandardsIgnoreLine ?></script>
+		<script src="assets/js/ancient.min.js" type="text/javascript"> <?php // @codingStandardsIgnoreLine ?></script>
+	</head>
 	<body>
 		<?php
-		// @codingStandardsIgnoreStart
-		echo '<pre>';
-		var_dump( $project_tasks );
-		echo '</pre>';
-		// @codingStandardsIgnoreEnd
+		foreach ( $project_tasks as $project => $tasks ) :
+			// Skip if no tasks.
+			if ( empty( $tasks ) ) {
+				continue;
+			}
+			?>
+			<div class="project">
+				<h2 class="project-title">
+					<?php echo $project; // @codingStandardsIgnoreLine ?>
+				</h2>
+				<div class="table">
+					<div class="th">
+						<div class="td">Task</div>
+						<div class="td">Assignee</div>
+						<div class="td">Due Date</div>
+						<div class="td">Last Update</div>
+						<div class="clear"></div>
+					</div>
+
+					<?php foreach ( $tasks as $task ) : ?>
+						<div class="tr">
+							<div class="td">
+								<?php echo $task->content; // @codingStandardsIgnoreLine ?>
+							</div>
+							<div class="td">
+								<?php isset( $task->assignee->name ) ? $task->assignee->name : ''; // @codingStandardsIgnoreLine ?>
+							</div>
+							<div class="td">
+								<?php echo $task->due_on; // @codingStandardsIgnoreLine ?>
+							</div>
+							<div class="td">
+								<?php echo date( 'Y-m-d', strtotime( $task->updated_at ) ); // @codingStandardsIgnoreLine ?>
+							</div>
+							<div style="clear: both;"></div>
+						</div>
+					<?php endforeach; ?>
+				</div>
+			</div>
+			<?php
+		endforeach;
 		?>
 	</body>
 </html>
